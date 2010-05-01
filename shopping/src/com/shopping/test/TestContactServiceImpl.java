e474acf3f7a14b26e7d143d4c59984dbde7f2743
dragon.domili@gmail.com <dragon.domili@gmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 14:36:06 +0000

    添加serviceimpl的测试

---
+++ shopping/src/com/shopping/test/TestContactServiceImpl.java
@@ -0,0 +1,73 @@
+package com.shopping.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.shopping.service.ContactService;
+import com.shopping.service.impl.ContactServiceImpl;
+import com.shopping.vo.ContactVo;
+
+public class TestContactServiceImpl {
+
+	private ContactService service = new ContactServiceImpl();
+	@Test
+	public void testAddContactDao() {
+		ContactVo contact = new ContactVo();
+		contact.setOrderId(1);
+		contact.setName("abc");
+		contact.setPostcode("123456");
+		contact.setAddress("abcabd");
+		contact.setTelphone("123456");
+		contact.setFreetime(1);
+		
+		assertTrue(service.addContact(contact));
+	}
+
+	@Test
+	public void testFindAllContact() {
+		List<ContactVo> list = new ArrayList<ContactVo>();
+		int start = 1;
+		int limit = 2;
+		list = service.findAllContact(start, limit);
+		assertNotNull(list);
+	}
+
+	@Test
+	public void testFindContactById() {
+		ContactVo contact = new ContactVo();
+		contact = service.findContactById(1);
+		assertNotNull(contact);
+	}
+
+	@Test
+	public void testFindContactByOrderId() {
+		ContactVo contact = new ContactVo();
+		contact = service.findContactByOrderId(1);
+		assertNotNull(contact);
+	}
+
+	@Test
+	public void testModifyContact() {
+
+		ContactVo contact = new ContactVo();
+		contact.setConId(1);
+		contact.setOrderId(1);
+		contact.setName("abc");
+		contact.setPostcode("123456");
+		contact.setAddress("abcabd");
+		contact.setTelphone("123456");
+		contact.setFreetime(1);
+		
+		assertTrue(service.modifyContact(contact));
+	}
+
+	@Test
+	public void testRemoveContactById() {
+		assertTrue(service.removeContactById(1));
+	}
+
+}
