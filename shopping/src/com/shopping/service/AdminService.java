a48d7dc93e8a99832b72598ca6ff525e729f4eb2
hanxue_lang@foxmail.com <hanxue_lang@foxmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 10:33:19 +0000

    还忘了提交接口，我晕啊

---
+++ shopping/src/com/shopping/service/AdminService.java
@@ -0,0 +1,58 @@
+package com.shopping.service;
+
+import java.util.List;
+
+import com.shopping.vo.AdminVo;
+
+public interface AdminService {
+	/**
+	 * 添加管理员
+	 * 
+	 * @param admin
+	 * @return boolean 是否添加成功
+	 */
+	public boolean addAdmin(AdminVo admin);
+
+	/**
+	 * 按ID删除管理员
+	 * 
+	 * @param id
+	 * @return boolean 是否删除成功
+	 */
+	public boolean removeAdminById(int id);
+
+	/**
+	 * 修改管理员信息
+	 * 
+	 * @param admin
+	 * @return boolean 是否修改成功
+	 */
+	public boolean modifyAdmin(AdminVo admin);
+
+	/**
+	 * 按ID查询出管理员
+	 * 
+	 * @param id
+	 * @return AdminVo
+	 */
+	public AdminVo findAdminById(int id);
+
+	/**
+	 * 查询全部分了页啦
+	 * 
+	 * @param start
+	 *            int 开始位置
+	 * @param limit
+	 *            int 偏移量
+	 * @return list 返回的符合条件的Admin
+	 */
+	public List<AdminVo> findAllAdmin(int start, int limit);
+
+	/**
+	 * 判断是否能登录成功
+	 * 
+	 * @param admin
+	 * @return boolean 是否登录成功
+	 */
+	public boolean isLogin(AdminVo admin);
+}
