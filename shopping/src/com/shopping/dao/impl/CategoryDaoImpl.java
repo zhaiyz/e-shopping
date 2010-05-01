0d1112d616644e1d4b49ed77099198493f01013a
dragon.domili@gmail.com <dragon.domili@gmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 09:58:54 +0000

    提交settings文件夹和三个imp

---
+++ shopping/src/com/shopping/dao/impl/CategoryDaoImpl.java
@@ -0,0 +1,157 @@
+package com.shopping.dao.impl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shopping.dao.CategoryDao;
+import com.shopping.util.DBUtil;
+import com.shopping.vo.CategoryVo;
+
+public class CategoryDaoImpl implements CategoryDao {
+
+	public boolean addCategory(CategoryVo category) {
+		boolean flag = false;
+		String sql = "INSERT INTO category (cat_name,cat_desc,cat_datetime) VALUES (?,?,NOW())";
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			pstmt.setString(1, category.getCatName());
+			pstmt.setString(2, category.getCatDesc());
+			if (pstmt.executeUpdate() == 1) {
+				flag = true;
+			}
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+	public List<CategoryVo> findAllCategory(int start, int limit) {
+		List<CategoryVo> list = new ArrayList<CategoryVo>();
+		String sql = "SELECT * FROM category WHERE 1 = 1 LIMIT" + start + ","
+				+ limit;
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				CategoryVo category = new CategoryVo();
+				category.setCatId(rs.getInt("cat_id"));
+				category.setCatName(rs.getString("cat_name"));
+				category.setCatDesc(rs.getString("cat_desc"));
+				category.setCatDatetime(rs.getDate("cat_datetime"));
+				list.add(category);
+			}
+			rs.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return list;
+	}
+
+	public CategoryVo findCategoryById(int catId) {
+		CategoryVo category = new CategoryVo();
+		String sql = "SELECT * FROM category WHERE cat_id = ? and 1 = 1";
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setInt(1, catId);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				category.setCatId(rs.getInt("cat_id"));
+				category.setCatName(rs.getString("cat_name"));
+				category.setCatDesc(rs.getString("cat_desc"));
+				category.setCatDatetime(rs.getDate("cat_datetime"));
+			}
+			rs.close();
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+
+		return category;
+	}
+
+	public CategoryVo findCategoryByName(String catName) {
+		CategoryVo category = new CategoryVo();
+		String sql = "SELECT * FROM category WHERE cat_name = ?";
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setString(1, catName);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				category.setCatId(rs.getInt("cat_id"));
+				category.setCatName(rs.getString("cat_name"));
+				category.setCatDesc(rs.getString("cat_desc"));
+				category.setCatDatetime(rs.getDate("cat_datetime"));
+			}
+			rs.close();
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return category;
+	}
+
+	public boolean modifyCategory(CategoryVo category) {
+		boolean flag = false;
+		String sql = "UPDATE category SET cat_name = ?, cat_desc = ?";
+		PreparedStatement pst = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setString(1, category.getCatName());
+			pst.setString(2, category.getCatDesc());
+			if (pst.executeUpdate() == 0 || pst.executeUpdate() == 1) {
+				flag = true;
+			}
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+	public boolean removeCategoryById(int catId) {
+		boolean flag = false;
+		String sql = "DELETE FROM category WHERE cat_id = ?";
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			pstmt.setInt(1, catId);
+			if (pstmt.executeUpdate() == 1) {
+				flag = true;
+			}
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+}
---
+++ shopping/src/com/shopping/dao/impl/ProductDaoImp.java
@@ -0,0 +1,236 @@
+package com.shopping.dao.impl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shopping.dao.ProductDao;
+import com.shopping.util.DBUtil;
+import com.shopping.vo.ProductVo;
+
+public class ProductDaoImp implements ProductDao {
+
+	public boolean addProduct(ProductVo product) {
+		boolean flag = false;
+		String sql = "INSERT INTO product (item_id, pro_name, imageurl, pro_desc, "
+				+ "pro_datetime, pru_price, ori_price, dis_price, stock, sales, "
+				+ "recommendation) VALUES (?,?,?,?,NOW(),?,?,?,?,?)";
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			pstmt.setInt(1, product.getItemId());
+			pstmt.setString(2, product.getProName());
+			pstmt.setString(3, product.getImageUrl());
+			pstmt.setString(4, product.getProDesc());
+			pstmt.setFloat(5, product.getPurPrice());
+			pstmt.setFloat(6, product.getOriPrice());
+			pstmt.setFloat(7, product.getDisPrice());
+			pstmt.setInt(8, product.getStock());
+			pstmt.setInt(9, product.getSales());
+			if (pstmt.executeUpdate() == 1) {
+				flag = true;
+			}
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+	public List<ProductVo> findAllProduct(int itemId, int start, int limit) {
+		List<ProductVo> list = new ArrayList<ProductVo>();
+		String sql = "SELECT * FROM product WHERE item_id = ? and 1 = 1 LIMIT"
+				+ start + "," + limit;
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			pstmt.setInt(1, itemId);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				ProductVo product = new ProductVo();
+				product.setProId(rs.getInt("pro_id"));
+				product.setItemId(rs.getInt("item_id"));
+				product.setProName(rs.getString("pro_name"));
+				product.setImageUrl(rs.getString("imageurl"));
+				product.setProDesc(rs.getString("pro_desc"));
+				product.setProDatetime(rs.getDate("pro_datetime"));
+				product.setPurPrice(rs.getFloat("pru_price"));
+				product.setOriPrice(rs.getFloat("ori_price"));
+				product.setDisPrice(rs.getFloat("dis_price"));
+				product.setStock(rs.getInt("stock"));
+				product.setSales(rs.getInt("sales"));
+				product.setRecommendation(rs.getInt("recommendation"));
+				list.add(product);
+			}
+			rs.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return list;
+	}
+
+	public List<ProductVo> findAllProduct(int start, int limit) {
+		List<ProductVo> list = new ArrayList<ProductVo>();
+		String sql = "SELECT * FROM product WHERE 1 = 1 LIMIT" + start + ","
+				+ limit;
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				ProductVo product = new ProductVo();
+				product.setProId(rs.getInt("pro_id"));
+				product.setItemId(rs.getInt("item_id"));
+				product.setProName(rs.getString("pro_name"));
+				product.setImageUrl(rs.getString("imageurl"));
+				product.setProDesc(rs.getString("pro_desc"));
+				product.setProDatetime(rs.getDate("pro_datetime"));
+				product.setPurPrice(rs.getFloat("pru_price"));
+				product.setOriPrice(rs.getFloat("ori_price"));
+				product.setDisPrice(rs.getFloat("dis_price"));
+				product.setStock(rs.getInt("stock"));
+				product.setSales(rs.getInt("sales"));
+				product.setRecommendation(rs.getInt("recommendation"));
+				list.add(product);
+			}
+			rs.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return list;
+	}
+
+	public ProductVo findProductById(int proId) {
+		ProductVo product = new ProductVo();
+		String sql = "SELECT * FROM product WHERE pro_id = ? and 1 = 1";
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setInt(1, proId);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				product.setProId(rs.getInt("pro_id"));
+				product.setItemId(rs.getInt("item_id"));
+				product.setProName(rs.getString("pro_name"));
+				product.setImageUrl(rs.getString("imageurl"));
+				product.setProDesc(rs.getString("pro_desc"));
+				product.setProDatetime(rs.getDate("pro_datetime"));
+				product.setPurPrice(rs.getFloat("pru_price"));
+				product.setOriPrice(rs.getFloat("ori_price"));
+				product.setDisPrice(rs.getFloat("dis_price"));
+				product.setStock(rs.getInt("stock"));
+				product.setSales(rs.getInt("sales"));
+				product.setRecommendation(rs.getInt("recommendation"));
+			}
+			rs.close();
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+
+		return product;
+	}
+
+	public ProductVo findProductByName(String proName) {
+		ProductVo product = new ProductVo();
+		String sql = "SELECT * FROM product WHERE pro_name = ?";
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setString(1, proName);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				product.setProId(rs.getInt("pro_id"));
+				product.setItemId(rs.getInt("item_id"));
+				product.setProName(rs.getString("pro_name"));
+				product.setImageUrl(rs.getString("imageurl"));
+				product.setProDesc(rs.getString("pro_desc"));
+				product.setProDatetime(rs.getDate("pro_datetime"));
+				product.setPurPrice(rs.getFloat("pru_price"));
+				product.setOriPrice(rs.getFloat("ori_price"));
+				product.setDisPrice(rs.getFloat("dis_price"));
+				product.setStock(rs.getInt("stock"));
+				product.setSales(rs.getInt("sales"));
+				product.setRecommendation(rs.getInt("recommendation"));
+			}
+			rs.close();
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return product;
+	}
+
+	public boolean modifyProduct(ProductVo product) {
+		boolean flag = false;
+		String sql = "UPDATE product SET item_id = ?, pro_name = ?, imageurl = ?, "
+				+ "pro_desc = ?, pru_price = ?, ori_price = ?, dis_price = ?,stock = ?,"
+				+ " sales = ?,recommendation = ?";
+		PreparedStatement pst = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pst = dbc.getConnection().prepareStatement(sql);
+			pst.setInt(1, product.getItemId());
+			pst.setString(2, product.getProName());
+			pst.setString(3, product.getImageUrl());
+			pst.setString(4, product.getProDesc());
+			pst.setFloat(5, product.getPurPrice());
+			pst.setFloat(6, product.getOriPrice());
+			pst.setFloat(7, product.getDisPrice());
+			pst.setInt(8, product.getStock());
+			pst.setInt(9, product.getSales());
+			pst.setInt(10, product.getRecommendation());
+			if (pst.executeUpdate() == 0 || pst.executeUpdate() == 1) {
+				flag = true;
+			}
+			pst.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+	public boolean removeProduct(int proId) {
+		boolean flag = false;
+		String sql = "DELETE FROM product WHERE pro_id = ?";
+		PreparedStatement pstmt = null;
+		DBUtil dbc = new DBUtil();
+		try {
+			pstmt = dbc.getConnection().prepareStatement(sql);
+			pstmt.setInt(1, proId);
+			if (pstmt.executeUpdate() == 1) {
+				flag = true;
+			}
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			dbc.close();
+		}
+		return flag;
+	}
+
+}
