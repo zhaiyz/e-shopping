00c8b279e157fd86beaddb3f337e016666e22b53
dragon.domili@gmail.com <dragon.domili@gmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 04:57:07 +0000

    添加Dao·········

---
+++ trunk/shopping/src/com/shopping/dao/ProductDao.java
@@ -0,0 +1,77 @@
+package com.shopping.dao;
+
+import java.util.List;
+
+import com.shopping.vo.ProductVo;
+
+public interface ProductDao {
+
+	/**
+	 * 添加商品
+	 * 
+	 * @param product
+	 *            商品对象
+	 * @return 添加是否成功
+	 */
+	public boolean addProduct(ProductVo product);
+
+	/**
+	 * 修改商品信息
+	 * 
+	 * @param product
+	 *            商品对象
+	 * @return 修改是否成功
+	 */
+	public boolean modifyProduct(ProductVo product);
+
+	/**
+	 * 删除商品
+	 * 
+	 * @param proId
+	 *            商品ID
+	 * @return 删除是否成功
+	 */
+	public boolean removeProduct(int proId);
+
+	/**
+	 * 分页查询所有商品
+	 * 
+	 * @param itemId
+	 *            商品所属小类的ID
+	 * @param start
+	 *            查询起始位置
+	 * @param limit
+	 *            分页显示的数量
+	 * @return 查询出的商品集合
+	 */
+	public List<ProductVo> findAllProduct(int itemId, int start, int limit);
+
+	/**
+	 * 分页查询所有商品
+	 * 
+	 * @param start
+	 *            起始查询位置
+	 * @param limit
+	 *            分页显示数量
+	 * @return 查询出的商品集合
+	 */
+	public List<ProductVo> findAllProduct(int start, int limit);
+
+	/**
+	 * 按商品ID查询商品
+	 * 
+	 * @param proId
+	 *            商品ID
+	 * @return 查询出的商品
+	 */
+	public ProductVo findProductById(int proId);
+
+	/**
+	 * 按名称查询商品
+	 * 
+	 * @param proName
+	 *            商品名称
+	 * @return 商品对象
+	 */
+	public ProductVo findProductByName(String proName);
+}
