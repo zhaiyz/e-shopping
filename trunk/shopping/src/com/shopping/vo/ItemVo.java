d718bdea62a684c5a66f4fa90418e0146829ec20
wengkeling@163.com <wengkeling@163.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 02:59:00 +0000

    添加商品小类Vo····

---
+++ trunk/shopping/src/com/shopping/vo/ItemVo.java
@@ -0,0 +1,53 @@
+package com.shopping.vo;
+
+import java.util.Date;
+
+/**
+ * @author 枫红叶落
+ *商品小类Vo
+ */
+public class ItemVo {
+	/**商品小类ID*/
+	private int itemId;
+	/**商品大类ID*/
+	private int catId;
+	/**商品小类名称*/
+	private String itemName;
+	/**商品小类描述*/
+	private String itemDesc;
+	/**添加商品日期*/
+	private Date itemDatetime;
+	public int getCatId() {
+		return catId;
+	}
+	public void setCatId(int catId) {
+		this.catId = catId;
+	}
+	public Date getItemDatetime() {
+		return itemDatetime;
+	}
+	public void setItemDatetime(Date itemDatetime) {
+		this.itemDatetime = itemDatetime;
+	}
+	public String getItemDesc() {
+		return itemDesc;
+	}
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	
+	
+
+}
