7b5778dd9ad7bf545ecd6ddfbba34f459b313fa7
hanxue_lang@foxmail.com <hanxue_lang@foxmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-01 10:27:02 +0000

    添加了四个service实现

---
+++ trunk/shopping/src/com/shopping/service/impl/OrderInfoServiceImpl.java
@@ -0,0 +1,38 @@
+package com.shopping.service.impl;
+
+import java.util.List;
+
+import com.shopping.dao.OrderInfoDao;
+import com.shopping.dao.impl.OrderInfoDaoImpl;
+import com.shopping.service.OrderInfoService;
+import com.shopping.vo.OrderInfoVo;
+
+public class OrderInfoServiceImpl implements OrderInfoService {
+	
+	private OrderInfoDao dao = new OrderInfoDaoImpl();
+
+	public boolean addOrderInfo(OrderInfoVo info) {
+		return dao.addOrderInfo(info);
+	}
+
+	public List<OrderInfoVo> findAllOrderInfo(int start, int limit) {
+		return dao.findAllOrderInfo(start, limit);
+	}
+
+	public OrderInfoVo findOrderInfoById(int id) {
+		return dao.findOrderInfoById(id);
+	}
+
+	public List<OrderInfoVo> findOrderInfoByOrderId(int id) {
+		return dao.findOrderInfoByOrderId(id);
+	}
+
+	public boolean modifyOrderInfo(OrderInfoVo info) {
+		return dao.modifyOrderInfo(info);
+	}
+
+	public boolean removeOrderInfoById(int id) {
+		return dao.removeOrderInfoById(id);
+	}
+
+}
