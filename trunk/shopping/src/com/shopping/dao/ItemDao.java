427625c032805d579ec87ae4233145f9215d3185
dragon.domili@gmail.com <dragon.domili@gmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-02 03:21:06 +0000

    Item中添加一按大类查询的方法，并测试

--- trunk/shopping/src/com/shopping/dao/ItemDao.java
+++ trunk/shopping/src/com/shopping/dao/ItemDao.java
@@ -27,6 +27,15 @@ public interface ItemDao {
 	 * @return 商品小类的对象
 	 */
 	public ItemVo findById(int itemId);
+	
+	/**
+	 * 按商品大类ID查询商品小类，不分页
+	 * @param catId
+	 * 商品大类ID
+	 * @return
+	 * 查询出的商品小类集合
+	 */
+	public List<ItemVo> findItemByCategoryId(int catId);
 
 	/**
 	 * 分页查询商品小类
