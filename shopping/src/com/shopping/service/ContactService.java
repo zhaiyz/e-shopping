8054e2a1f74d95f38a433c30b2a751f0a2f8f2e9
hanxue_lang@foxmail.com <hanxue_lang@foxmail.com@fa5c98e0-2c54-11df-afb1-bd2833558206>
2010-05-02 02:13:59 +0000

    修改乱码，原来UTF-8，GBK都乱，现在是UTF-8

--- shopping/src/com/shopping/service/ContactService.java
+++ shopping/src/com/shopping/service/ContactService.java
@@ -8,55 +8,47 @@ import com.shopping.vo.ContactVo;
  * ContactService
  */
 public interface ContactService {
+	
 	/**
-	 * ���һ����͵�ַ
-	 * 
+	 * 添加寄送地址信息
 	 * @param contact
-	 * @return boolean �Ƿ���ӳɹ�
+	 * @return 是否添加成功
 	 */
 	public boolean addContact(ContactVo contact);
-
+	
 	/**
-	 * ��IDɾ����͵�ַ
-	 * 
+	 * 按id删除寄送地址信息
 	 * @param id
-	 * @return boolean �Ƿ�ɾ��ɹ�
+	 * @return 是否删除成功
 	 */
 	public boolean removeContactById(int id);
-
+	
 	/**
-	 * �޸ļ��͵�ַ
-	 * 
+	 * 修改寄送地址信息
 	 * @param contact
-	 * @return boolean �Ƿ��޸ĳɹ�
+	 * @return 是否修改成功
 	 */
 	public boolean modifyContact(ContactVo contact);
-
+	
 	/**
-	 * ��ѯ��ȫ��jϵ��ַ
-	 * 
-	 * @param start
-	 *            int ��ʼλ��
-	 * @param limit
-	 *            int ƫ��
-	 * @return list ��������Contact��¼
+	 * 查询所以寄送地址信息并分页
+	 * @param start 开始位置
+	 * @param limit 偏移量
+	 * @return 符合条件的所有Contact记录
 	 */
 	public List<ContactVo> findAllContact(int start, int limit);
-
+	
 	/**
-	 * ��ID��ѯContact
-	 * 
+	 * 按id查询寄送地址信息
 	 * @param id
-	 * @return ContactVo
+	 * @return 寄送地址
 	 */
 	public ContactVo findContactById(int id);
-
+	
 	/**
-	 * ��OrderId��ѯ����Ӧ��Contact��¼
-	 * 
+	 * 按订单号查询寄送地址信息
 	 * @param id
-	 *            orderId ����ID
-	 * @return ContactVo ��Ӧ����ID�Ķ������͵�ַ
+	 * @return 寄送地址
 	 */
 	public ContactVo findContactByOrderId(int id);
 }
